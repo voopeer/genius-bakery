1be7b7f0f119c0590eaa44d58fcf8baca7293f54
voopeer <151393232+voopeer@users.noreply.github.com>
2025-03-26 14:36:10 +0200

    Add files via upload

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,13 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-container', {
 
   loop: true,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    }
+  },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination',
+    bullerClass: 'pagination__button',
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
   },
 });
